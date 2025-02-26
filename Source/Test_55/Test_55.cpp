31795b45f6879489183daa6a444588e12582f2ef
Gleb <hmarsky.gleb@yandex.ru>
2025-02-27 01:38:25 +0200

    [C++ *Regenerate] add cpp

---
+++ Source/Test_55/Test_55.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Test_55.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Test_55, "Test_55" );
